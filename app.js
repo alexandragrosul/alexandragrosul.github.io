e11a56bd90c47e59f49d213ded437ac6e374149c
alexandragrosul <73661926+alexandragrosul@users.noreply.github.com>
2022-05-01 17:23:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
-const sun = document.getElementsByClassName('btn-toggle')[0];
-const moon = document.getElementsByClassName('btn-toggle')[1];
-sun.addEventListener("click", function () {
+const themeSwitch = document.getElementById("themeSwitch");
+const themeIconPath = themeSwitch.src.split('/')
+const themeIconImg = themeIconPath[themeIconPath.length - 1]
+console.log(localStorage.getItem('isDark'))
+let isDarkTheme = false
+if(localStorage.getItem('isDark') === null){
+    localStorage.setItem('isDark', 'false')
+} else {
+    isDarkTheme = JSON.parse(localStorage.getItem('isDark'))
+}
+
+if(isDarkTheme){
+    themeSwitch.src="images/sun.png"
     document.body.classList.toggle('dark-theme');
-    //document.header.classList.toggle('dark-theme');
-})
-moon.addEventListener("click", function () {
+}
+// const moon = document.getElementsByClassName('btn-toggle')[1];
+themeSwitch.addEventListener("click", function () {
+
+    console.log(isDarkTheme)
+    isDarkTheme = !isDarkTheme
+    localStorage.setItem('isDark',isDarkTheme ? 'true' : 'false')
+    isDarkTheme ? themeSwitch.src="images/sun.png" : themeSwitch.src="images/moon.png"
+
+    
     document.body.classList.toggle('dark-theme');
-    // document.body.header.classList.toggle('dark-theme');
 })
+
+const photoGallery = {
+    cat : null,
+    picture : null,
+    sculpture : null
+}
+
+
+
+/**
+ * Build arr of images for gallery.
+ * @param {array} list  - The colection of images.
+ */
+const buildGallery = (list) => {
+    console.log(list)
+    let result = []
+    for (const image of list) {
+        result.push(`<a data-fancybox='gallery' href='${image.link}'><img src='${image.link}' alt='${image.title}'></a>`)
+    
+    }
+    return result;
+}
+
+fetch("gallery.json")
+  .then(response => response.json())
+  .then(json => {
+      photoGallery.cat = json.cat
+      photoGallery.picture = json.picture
+      photoGallery.sculpture = json.sculpture
+      console.dir(buildGallery(photoGallery.cat));
+  });
+
+
+ 
