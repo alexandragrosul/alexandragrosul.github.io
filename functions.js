15dd267d8b603b669ae871397c04e77a493a61c1
alexandragrosul <73661926+alexandragrosul@users.noreply.github.com>
2022-05-03 21:33:48 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,24 @@
+function sum(a, b) {
+    return a + b;
+  }
+
+/**
+ * Build arr of images for gallery.
+ * @param {array} list  - The colection of images.
+ */  
+export const buildGallery = (list) => {
+    let result = []
+    for (const image of list) {
+        result.push(`<a data-fancybox='gallery' href='${image.link}'><img src='${image.link}' alt='${image.title}'></a>`)
+    }
+    return result;
+}
+
+
+
+export const topFunction = () => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+//   module.exports = sum;
+//   module.exports = buildGallery;
